b987992d0c289ff8e244d0354eb35c4f94bd3813
Varun T <vt@akamai.com>
2021-05-30 09:43:59 +0530

    First commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import pandas as pd
 import requests
-from datetime import date,datetime, timedelta
+from datetime import date, datetime, timedelta
 import smtplib
 import json
 import http.client
@@ -20,46 +20,62 @@ timenow = datetime.now()
 host = "https://cdn-api.co-vin.in/api/"
 index = 1
 
+
 def isnamepresent(hosp_name, regex):
     if hosp_name.lower().find(regex.lower()) != -1:
         return True
     else:
         return False
 
-def makeRestCall(district_id,date_p):
+
+def makeRestCall(district_id, date_p):
     conn = http.client.HTTPSConnection("cdn-api.co-vin.in")
     payload = ''
     headers = {}
-    conn.request("GET", "/api/v2/appointment/sessions/public/findByDistrict?district_id="+district_id+"&date="+date_p, payload,
+    conn.request("GET",
+                 "/api/v2/appointment/sessions/public/findByDistrict?district_id=" + district_id + "&date=" + date_p,
+                 payload,
                  headers)
     res = conn.getresponse()
     data = res.read()
-    #print(data.decode("utf-8"))
+    # print(data)
+    # print(data.decode("utf-8"))
     return json.loads(data.decode('utf-8'))['sessions']
 
-def findVaccineByDistrict(district_id, date1=None, vaccine_type='COVAXIN', pincode=None, name_regex=None, buffer_days = 1,age_limit=45):
+
+def findVaccineByDistrict(district_id, date1=None, vaccine_type='COVAXIN', pincode=None, name_regex=None, buffer_days=1,
+                          age_limit=18, min_capacity=1, dose_type="first"):
     url = host + "v2/appointment/sessions/public/findByDistrict?"
     today = date.today()
     timenow = datetime.now()
-    for buf_days in range(0,buffer_days):
+    for buf_days in range(0, buffer_days):
         if buf_days > 0:
             today = today + timedelta(days=1)
             date1 = today.strftime("%d-%m-%Y")
             print("Checking for next date", date1)
-        elif buf_days==0:
+        elif buf_days == 0:
             date1 = today.strftime("%d-%m-%Y")
             print("Checking for ", date1)
 
         params = {'district_id': district_id, "date": date1}
         try:
-            #response = requests.get(url=url, params=params)
-            #data = response.json()['sessions']
-            data = makeRestCall(district_id,date1)
-            #data = []
+            # response = requests.get(url=url, params=params)
+            # data = response.json()['sessions']
+            data = makeRestCall(district_id, date1)
+            # data = []
         except Exception as e1:
             print(data)
             raise e1
         filter_data = data
+        if dose_type == "first":
+            filter_data = list(
+                filter(lambda hospital: hospital['available_capacity_dose1'] >= min_capacity, filter_data))
+        elif dose_type == "second":
+            filter_data = list(
+                filter(lambda hospital: hospital['available_capacity_dose2'] >= min_capacity, filter_data))
+        else:
+            filter_data = list(
+                filter(lambda hospital: hospital['available_capacity_dose'] >= min_capacity, filter_data))
         if vaccine_type is not None:
             filter_data = list(filter(lambda hospital: hospital['vaccine'] == vaccine_type, filter_data))
         if pincode is not None:
@@ -81,26 +97,30 @@ def findVaccineByDistrict(district_id, date1=None, vaccine_type='COVAXIN', pinco
             html = df.to_html()
             email_body = json.dumps(filter_data)
             send_email(html)
-            print("----"*10)
+            print("----" * 10)
             for hosp in filter_data:
-                print("Hospital name:",hosp['name'],"available_capacity:",hosp['available_capacity'],"pincode:",hosp['pincode'],"area:",hosp['block_name'])
+                print("Hospital name:", hosp['name'], "available_capacity:", hosp['available_capacity'], "pincode:",
+                      hosp['pincode'], "area:", hosp['block_name'])
             print("----" * 10)
         else:
             print("No hospital found")
 
 
-def send_email(email_response, email_address=['varunvt119@gmail.com', 'bhargavivt1@gmail.com', 'vstapas@gmail.com']):
+#
+def send_email(email_response, email_address=None):
+    if email_address is None:
+        email_address = ['varunvt119@gmail.com', 'abhishek2gargcochin@gmail.com', 'cancernav26@gmail.com']
     part2 = MIMEText(email_response, 'html')
     msg = MIMEMultipart('alternative')
     global index
     timenow = datetime.now()
     msg['Subject'] = timenow.strftime("%m/%d/%Y, %H:%M:%S") + ': Vaccine Finder Results #' + str(index)
 
-    index = index +1
+    index = index + 1
     msg['From'] = gmail_user
     msg['To'] = ", ".join(email_address)
     msg.attach(part2)
-    #print("sending email to", ", ".email_address)
+    # print("sending email to", ", ".email_address)
     try:
         smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
         smtpserver.ehlo()
@@ -122,16 +142,17 @@ if __name__ == '__main__':
     # vaccine_type = "COVAXIN"
     # hospital name = "MANIPAL NORTHSIDE CANARA UNIO"
     import time
+
     while True:
         try:
-            #findVaccineByDistrict('294', vaccine_type='COVAXIN')
-            findVaccineByDistrict('294', vaccine_type='COVAXIN',buffer_days=2)
+            # findVaccineByDistrict('294', vaccine_type='COVAXIN')
+            findVaccineByDistrict('294', vaccine_type='COVAXIN', buffer_days=5)
             time.sleep(30)
             print("sleeping")
             print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
         except Exception as e:
-            #import traceback as t
-            #t.print_exc(e)
+            # import traceback as t
+            # t.print_exc(e)
             traceback.print_exc()
             print(datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
             print("Continuing....")
